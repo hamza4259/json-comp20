aa9e67b164c5579b5632d58d3f89f53f9dda4955
Hamza Ali <hamza4259@yahoo.com>
2021-11-01 14:56:25 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+// for global access inside of filterByGenre
+let jsonObject;
+
+// mimic api call
+$(document).ready(() => {
+  $.get(
+    "https://raw.githubusercontent.com/hamza4259/json-comp20/main/setlist.json",
+    function(data) {
+      jsonObject = JSON.parse(data);
+      let jsonString = JSON.stringify(jsonObject);
+      const rawElemSelector = document.querySelector("#raw-json-string");
+      rawElemSelector.innerHTML = jsonString;
+
+      // by default we want to display all of the cards
+      let allCards = document.querySelector("#all-cards"); 
+      
+      // song is each individual object in the whole json object
+      jsonObject.forEach((song) => {
+        let songCard = document.createElement("div");
+        songCard.className = "song-card";
+        songCard.innerHTML = `<div>Title: ${song.Artist}</div><div>Artist: ${song.Title}</div>`;
+        songCard.innerHTML += `<div>Genre: ${song.Genre}</div><div>Year: ${song.Year}</div>`;
+        allCards.appendChild(songCard);
+      });
+	});
+});
+
+
+const filterByGenre = (e) => {
+  const selectedGenre = $("#genres").val();
+  const allCardsSelector = document.querySelector("#all-cards");
+  allCardsSelector.innerHTML = "";
+
+  // for each object in the JSON check to see if its of appropriate genre
+  // then just add to the div created in the html
+  jsonObject.forEach((song) => {
+    if (song.Genre == selectedGenre || selectedGenre == "All") {
+      let songCard = document.createElement("div");
+      songCard.className = "song-card";
+      songCard.innerHTML = `<div>Title: ${song.Artist}</div><div>Artist: ${song.Title}</div>`;
+      songCard.innerHTML += `<div>Genre: ${song.Genre}</div><div>Year: ${song.Year}</div>`;
+      allCardsSelector.appendChild(songCard);
+    }
+  });
+};
